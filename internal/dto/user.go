361cac77bedb8bd871f38aeee81ef47ddc31fc23
Lontor <ilyassakaev@gmail.com>
2024-11-01 23:28:06 +0300

    Removed UserResponse DTO

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -1,19 +1,8 @@
 package dto
 
 import (
-	"time"
-
 	"github.com/Lontor/todo-api/internal/model"
-	"github.com/google/uuid"
 )
-
-// GET /users/{userID}
-type UserResponse struct {
-	ID          uuid.UUID      `json:"id"`
-	Email       string         `json:"email"`
-	AccountType model.UserType `json:"accountType"`
-	CreatedAt   time.Time      `json:"createdAt"`
-}
 
 // PUT /users/{userID}
 type UpdateUserRequest struct {
