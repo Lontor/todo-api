e10ef586218b6cd01d5d402035f46594dec0e727
Lontor <ilyassakaev@gmail.com>
2024-11-01 23:29:24 +0300

    Added user service interface

---
+++ internal/service/user_service.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Lontor/todo-api/internal/dto"
+	"github.com/Lontor/todo-api/internal/model"
+	"github.com/google/uuid"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, data dto.RegisterRequest) error
+	GetUserByID(ctx context.Context, id uuid.UUID) (model.User, error)
+	UpdateUser(ctx context.Context, data dto.UpdateUserRequest) error
+	DeleteUser(ctx context.Context, id uuid.UUID) error
+	AuthenticateUser(ctx context.Context, email, password string) (dto.AuthResponse, error)
+}
