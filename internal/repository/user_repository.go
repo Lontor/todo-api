f248323e924bb1999c25377474adb2f9853aeed3
Lontor <ilyassakaev@gmail.com>
2024-11-01 22:51:56 +0300

    Added Get method

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -9,6 +9,7 @@ import (
 
 type UserRepository interface {
 	Create(ctx context.Context, user model.User) error
+	Get(ctx context.Context) ([]model.User, error)
 	GetByEmail(ctx context.Context, email string) (model.User, error)
 	GetByID(ctx context.Context, id uuid.UUID) (model.User, error)
 	Update(ctx context.Context, user model.User) error
