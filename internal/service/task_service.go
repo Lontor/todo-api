d85e41a85e92a7031f84c7342152d9b4428c56d8
Lontor <ilyassakaev@gmail.com>
2024-11-01 23:29:08 +0300

    Added task service interface

---
+++ internal/service/task_service.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"context"
+
+	"github.com/Lontor/todo-api/internal/dto"
+	"github.com/Lontor/todo-api/internal/model"
+	"github.com/google/uuid"
+)
+
+type TaskService interface {
+	CreateTask(ctx context.Context, data dto.CreateTaskRequest) error
+	GetTasksByUser(ctx context.Context, userID uuid.UUID, status model.TaskStatus) ([]model.Task, error)
+	GetTaskByID(ctx context.Context, id uuid.UUID) (model.Task, error)
+	UpdateTask(ctx context.Context, data dto.UpdateTaskRequest) error
+	DeleteTask(ctx context.Context, id uuid.UUID) error
+}
