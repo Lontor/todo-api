ba5d51d90ab974eaffa37afa8c9441a571a5658e
Lontor <ilyassakaev@gmail.com>
2024-11-02 21:00:21 +0300

    Added taskID and userID fields to dto

--- internal/dto/task.go
+++ internal/dto/task.go
@@ -1,15 +1,21 @@
 package dto
 
-import "github.com/Lontor/todo-api/internal/model"
+import (
+	"github.com/Lontor/todo-api/internal/model"
+	"github.com/google/uuid"
+)
 
 // POST /users/{userID}/tasks
 type CreateTaskRequest struct {
 	Description string           `json:"description" validate:"required,min=10,max=200"`
 	Status      model.TaskStatus `json:"status,omitempty" validate:"omitempty,oneof='to do' 'in progress' 'done'"`
+	UserID      uuid.UUID        `json:"userID" validate:"required"`
 }
 
 // PATCH /users/{userID}/tasks/{taskID}
 type UpdateTaskRequest struct {
 	Description *string           `json:"description,omitempty" validate:"omitempty,min=10,max=200"`
 	Status      *model.TaskStatus `json:"status,omitempty" validate:"omitempty,oneof='to do' 'in progress' 'done'"`
+	UserID      uuid.UUID         `json:"userID" validate:"required"`
+	TaskID      uuid.UUID         `json:"taskID" validate:"required"`
 }
--- internal/dto/user.go
+++ internal/dto/user.go
@@ -2,6 +2,7 @@ package dto
 
 import (
 	"github.com/Lontor/todo-api/internal/model"
+	"github.com/google/uuid"
 )
 
 // PUT /users/{userID}
@@ -9,4 +10,5 @@ type UpdateUserRequest struct {
 	Email    string         `json:"email,omitempty" validate:"omitempty,email"`
 	Password string         `json:"password,omitempty" validate:"omitempty,min=8"`
 	Role     model.UserType `json:"role,omitempty" validate:"omitempty,oneof=regular admin"`
+	UserID   uuid.UUID      `json:"userID" validate:"required"`
 }
