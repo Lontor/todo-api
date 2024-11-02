f94acd1223835921b19e58261cd2551565e429a5
Lontor <ilyassakaev@gmail.com>
2024-11-02 22:21:20 +0300

    Added user service implementation

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.23.2
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-playground/validator/v10 v10.22.1
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/stretchr/testify v1.9.0
+	golang.org/x/crypto v0.19.0
 	gorm.io/driver/sqlite v1.5.6
 	gorm.io/gorm v1.25.12
 )
@@ -21,7 +23,6 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ internal/service/user_service_impl.go
@@ -0,0 +1,142 @@
+package service
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/Lontor/todo-api/internal/dto"
+	"github.com/Lontor/todo-api/internal/model"
+	"github.com/Lontor/todo-api/internal/repository"
+	"github.com/Lontor/todo-api/pkg/custom_errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type userService struct {
+	r repository.UserRepository
+}
+
+func NewUserService(r *repository.UserRepository) UserService {
+	return &userService{*r}
+}
+
+func (s *userService) CreateUser(ctx context.Context, data dto.RegisterRequest) error {
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(data.Password), 14)
+
+	if err != nil {
+		return custom_errors.NewHTTPError(http.StatusInternalServerError, "hash generation error")
+	}
+
+	if role, ok := ctx.Value("role").(model.UserType); ok {
+		if role != model.UserTypeAdmin {
+			return custom_errors.NewHTTPError(http.StatusForbidden, "permission denied")
+		}
+		s.r.Create(ctx, model.User{
+			ID:           uuid.New(),
+			Email:        data.Email,
+			PasswordHash: string(passwordHash),
+			AccountType:  data.Role,
+		})
+	}
+
+	if data.Role != model.UserTypeRegular {
+		return custom_errors.NewHTTPError(http.StatusForbidden, "permission denied")
+	}
+
+	return s.r.Create(ctx, model.User{
+		ID:           uuid.New(),
+		Email:        data.Email,
+		PasswordHash: string(passwordHash),
+		AccountType:  data.Role,
+	})
+}
+
+func (s *userService) GetUsers(ctx context.Context) ([]model.User, error) {
+	role, ok := ctx.Value("role").(model.UserType)
+	if !ok || role != model.UserTypeAdmin {
+		return nil, custom_errors.NewHTTPError(http.StatusForbidden, "permission denied")
+	}
+	return s.r.Get(ctx)
+}
+
+func (s *userService) GetUserByID(ctx context.Context, id uuid.UUID) (model.User, error) {
+	userID := ctx.Value("userID").(uuid.UUID)
+	role := ctx.Value("role").(model.UserType)
+
+	if role != model.UserTypeAdmin {
+		if id != userID {
+			return model.User{}, custom_errors.NewHTTPError(http.StatusForbidden, "permission denied")
+		}
+	}
+
+	return s.r.GetByID(ctx, id)
+}
+
+func (s *userService) UpdateUser(ctx context.Context, data dto.UpdateUserRequest) error {
+	userID := ctx.Value("userID").(uuid.UUID)
+	role := ctx.Value("role").(model.UserType)
+
+	if role != model.UserTypeAdmin {
+		if userID != data.UserID || data.Role != model.UserTypeRegular {
+			return custom_errors.NewHTTPError(http.StatusForbidden, "permission denied")
+		}
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(data.Password), 14)
+
+	if err != nil {
+		return custom_errors.NewHTTPError(http.StatusInternalServerError, "hash generation error")
+	}
+
+	return s.r.Update(ctx, model.User{
+		ID:           data.UserID,
+		Email:        data.Email,
+		PasswordHash: string(passwordHash),
+		AccountType:  data.Role,
+	})
+}
+
+func (s *userService) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	userID := ctx.Value("userID").(uuid.UUID)
+	role := ctx.Value("role").(model.UserType)
+
+	if role != model.UserTypeAdmin {
+		if id != userID {
+			return custom_errors.NewHTTPError(http.StatusForbidden, "permission denied")
+		}
+	}
+
+	return s.r.Delete(ctx, id)
+}
+
+func (s *userService) AuthenticateUser(ctx context.Context, email, password string) (dto.AuthResponse, error) {
+	user, err := s.r.GetByEmail(ctx, email)
+	if err != nil {
+		return dto.AuthResponse{}, custom_errors.NewHTTPError(http.StatusNotFound, "user not found")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password)); err != nil {
+		return dto.AuthResponse{}, custom_errors.NewHTTPError(http.StatusUnauthorized, "invalid credentials")
+	}
+
+	expirationTime := time.Now().Add(time.Hour * 3)
+
+	claims := jwt.MapClaims{
+		"userID": user.ID.String(),
+		"role":   user.AccountType,
+		"exp":    expirationTime.Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte("Y80zXN/dnoc14mdIpchh4ZXOdDAZfWulff5jEcCWHEc="))
+	if err != nil {
+		return dto.AuthResponse{}, custom_errors.NewHTTPError(http.StatusInternalServerError, "token generation error")
+	}
+
+	return dto.AuthResponse{
+		Token:     tokenString,
+		ExpiresAt: expirationTime,
+	}, nil
+}
