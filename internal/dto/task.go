b58ef3d955dfa0ad38e5ca9a1adc91ca0e3527db
Lontor <ilyassakaev@gmail.com>
2024-11-03 19:40:54 +0300

    UpdateTaskRequest has been changed

--- internal/dto/task.go
+++ internal/dto/task.go
@@ -14,8 +14,8 @@ type CreateTaskRequest struct {
 
 // PATCH /users/{userID}/tasks/{taskID}
 type UpdateTaskRequest struct {
-	Description *string           `json:"description,omitempty" validate:"omitempty,min=10,max=200"`
-	Status      *model.TaskStatus `json:"status,omitempty" validate:"omitempty,oneof='to do' 'in progress' 'done'"`
-	UserID      uuid.UUID         `json:"userID" validate:"required"`
-	TaskID      uuid.UUID         `json:"taskID" validate:"required"`
+	Description string           `json:"description,omitempty" validate:"omitempty,min=10,max=200"`
+	Status      model.TaskStatus `json:"status,omitempty" validate:"omitempty,oneof='to do' 'in progress' 'done'"`
+	UserID      uuid.UUID        `json:"userID" validate:"required"`
+	TaskID      uuid.UUID        `json:"taskID" validate:"required"`
 }
