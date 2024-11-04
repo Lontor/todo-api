61fc2a4344122dffee041bf99ffab32896c80c5a
Lontor <ilyassakaev@gmail.com>
2024-11-04 18:18:58 +0300

    Added UserID field to AuthResponse

--- internal/dto/auth.go
+++ internal/dto/auth.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/Lontor/todo-api/internal/model"
+	"github.com/google/uuid"
 )
 
 // POST /auth/login
@@ -16,10 +17,11 @@ type LoginRequest struct {
 type RegisterRequest struct {
 	Email    string         `json:"email" validate:"required,email"`
 	Password string         `json:"password" validate:"required,min=8"`
-	Role     model.UserType `json:"role" validate:"omitempty,oneof=regular admin"`
+	Role     model.UserType `json:"role,omitempty" validate:"omitempty,oneof=regular admin"`
 }
 
 type AuthResponse struct {
 	Token     string    `json:"token"`
+	UserID    uuid.UUID `json:"userID"`
 	ExpiresAt time.Time `json:"expiresAt"`
 }
